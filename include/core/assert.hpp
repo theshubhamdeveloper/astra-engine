2c7b034cd65485f69e06dc4d2239a3ead64a8da6
shubham <shubham2010gupta.dev@gmail.com>
2026-04-20 15:51:54 +0530

    feat(core): add Assert for error check and debug

---
+++ include/core/assert.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+#ifdef DEBUG
+#define ASSERT(condition) if (!(condition)){                       \
+    std::cerr << "Assertion failed: "                              \
+    << #condition                                                  \
+    << " File: " << __FILE__<< " Line: " << __LINE__<< std::endl;  \
+    std::abort();                                                  \
+    }
+#else
+#define ASSERT(condition)
+#endif
