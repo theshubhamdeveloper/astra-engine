b02f76b27faaf6b33ede4ffb718d47dc0efd49cb
shubham <shubham2010gupta.dev@gmail.com>
2026-04-30 16:52:10 +0530

    feat(ecs): add Transform component

---
+++ include/ecs/components/transform.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "math/vec2.hpp"
+#include <cstdint>
+
+namespace astra::ecs::component {
+struct Transform {
+    math::Vec2 position;
+    math::Vec2 scale;
+    float rotation;
+    int32_t zindex;
+};
+}
