d3c3b919cfbefad6c3e402ac4307e8ab38c74bda
shubham <shubham2010gupta.dev@gmail.com>
2026-04-30 16:52:57 +0530

    feat(ecs): add Shape component

---
+++ include/ecs/components/shape.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "math/color.hpp"
+#include "math/vec2.hpp"
+#include <variant>
+
+namespace astra::ecs::component {
+
+struct RectangleGeometry {
+    math::Vec2 size;
+    float cornerRadius;
+};
+
+struct CircleGeometry {
+    float radius;
+};
+
+struct LineGeometry {
+    math::Vec2 start;
+    math::Vec2 end;
+};
+
+using ShapeGeometry = std::variant<RectangleGeometry, CircleGeometry, LineGeometry>;
+
+struct ShapeStyle {
+    math::Color fill;
+    math::Color stroke;
+    float strokeWidth;
+    float opacity;
+    bool display;
+};
+
+struct Shape {
+    ShapeGeometry geometry;
+    ShapeStyle style;
+};
+}
