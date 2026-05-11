d2884fb44fc039f94f7e81da8c7871014c20e9a0
shubham <shubham2010gupta.dev@gmail.com>
2026-05-12 01:42:10 +0530

    fix(math): resolve incorrect point inside triangle

--- include/math/geometry.hpp
+++ include/math/geometry.hpp
@@ -10,14 +10,10 @@ namespace astra::math {
         return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
     }
 
-    constexpr bool isPointRightSideOfLine(const Vec2 &a, const Vec2 &b, const Vec2 &p) {
-        return triangleEdge(a, b, p) < 0;
-    }
-
     inline bool isPointInsideTriangle(const Vec2 &a, const Vec2 &b, const Vec2 &c, const Vec2 &p) {
-        const bool sideAB = isPointRightSideOfLine(a, b, p);
-        const bool sideBC = isPointRightSideOfLine(b, c, p);
-        const bool sideCA = isPointRightSideOfLine(c, a, p);
+        const bool sideAB = triangleEdge(a, b, p) <= 0;
+        const bool sideBC = triangleEdge(b, c, p) <= 0;
+        const bool sideCA = triangleEdge(c, a, p) <= 0;
         return sideAB == sideBC && sideAB == sideCA;
     }
 
