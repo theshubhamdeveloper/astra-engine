6c506ba5551ce0c3bf79560ea7906a491822b2c1
shubham <shubham2010gupta.dev@gmail.com>
2026-05-15 19:19:36 +0530

    test(opengl): create first GPU triangle

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "SDL3/SDL_init.h"
 #include "SDL3/SDL_log.h"
+#include "glad/glad.h"
 #include "core/time.hpp"
 #include "ecs/world/world.hpp"
 #include "input/input.hpp"
@@ -9,6 +10,28 @@ using namespace astra;
 
 constexpr int SCREEN_WIDTH = 800;
 constexpr int SCREEN_HEIGHT = 600;
+
+auto vertexShaderSource = R"(
+#version 330 core
+
+layout(location = 0) in vec2 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos.x , aPos.y, 0.0, 1.0);
+}
+)";
+
+auto fragmentShaderSource = R"(
+#version 330 core
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+)";
 
 int main() {
     if (SDL_Init(SDL_INIT_VIDEO) == false) {
@@ -22,6 +45,55 @@ int main() {
     auto world = ecs::world::World();
 
     window.initialize(false);
+
+    float vertices[] =
+    {
+        0.0f, 0.5f,
+        -0.5f, -0.5f,
+        0.5f, -0.5f
+    };
+
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sizeof(vertices),
+        vertices,
+        GL_STATIC_DRAW
+    );
+
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glVertexAttribPointer(
+        0,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        2 * sizeof(float),
+        nullptr
+    );
+    glEnableVertexAttribArray(0);
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const char *vsSource = vertexShaderSource;
+    glShaderSource(vertexShader, 1, &vsSource, nullptr);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const char *fsSource = fragmentShaderSource;
+    glShaderSource(fragmentShader, 1, &fsSource, nullptr);
+    glCompileShader(fragmentShader);
+
+
+    GLuint shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
 
     //
     // auto renderer = render::Renderer();
@@ -42,6 +114,10 @@ int main() {
             running = false;
 
         window.clear({255, 255, 255});
+
+        glBindVertexArray(VAO);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // world.update(time.deltaTime());
 
