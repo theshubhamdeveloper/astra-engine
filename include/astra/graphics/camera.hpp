a95ca84aa91385f39d81507a7eb21b71c7ac6a5a
shubham <shubham2010gupta.dev@gmail.com>
2026-05-29 17:18:22 +0530

    feat(graphics): add GraphicCamera struct

---
+++ include/astra/graphics/camera.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <astra/math/mat3.hpp>
+#include <astra/math/vector.hpp>
+
+#include "astra/core/types.hpp"
+
+namespace astra::graphics {
+    struct GraphicCamera {
+        math::Vec2 position;
+        float rotation = 0.0f;
+        float zoom = 1.0f;
+        math::Mat3 projection;
+
+        [[nodiscard]] math::Mat3 getView() const {
+            return math::Mat3::scale(zoom, zoom) *
+                   math::Mat3::rotation(-rotation * core::RADIAN_CONVERSION_FACTOR) *
+                   math::Mat3::translation(-position.x, -position.y);
+        }
+
+        constexpr static math::Mat3 orthographic(const float left, const float right, const float bottom,
+                                                 const float top) {
+            return {
+                2.0f / (right - left), 0, -((right + left) / (right - left)),
+                0, 2.0f / (top - bottom), -((top + bottom) / (top - bottom)),
+                0, 0, 1
+            };
+        }
+    };
+}
