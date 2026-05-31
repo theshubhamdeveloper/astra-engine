db2fa5dc7003eebf02cc18988edd3be7d9e9b11f
shubham <shubham2010gupta.dev@gmail.com>
2026-06-01 01:04:59 +0530

    refactor(graphics): switch GL_NEAREST to GL_LINEAR

--- src/graphics/texture.cpp
+++ src/graphics/texture.cpp
@@ -11,8 +11,8 @@ namespace astra::graphics {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
                      image.pixels.data());
