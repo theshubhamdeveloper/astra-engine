efe2a24a83c70057d9c44a3d7ee6f4dfcb2d673b
shubham <shubham2010gupta.dev@gmail.com>
2026-07-09 14:40:29 +0530

    feat(core): add alias of unique_ptr and shader_ptr

--- include/astra/core/types.hpp
+++ include/astra/core/types.hpp
@@ -2,8 +2,25 @@
 
 #include <cstdint>
 #include <numbers>
+#include <memory>
 
 namespace astra::core {
     constexpr size_t INVALID_INDEX = SIZE_MAX;
     constexpr float RADIAN_CONVERSION_FACTOR = std::numbers::pi / 180.0f;
+
+    template<typename T>
+    using Ref = std::shared_ptr<T>;
+
+    template<typename T, typename... Args>
+    constexpr Ref<T> CreateRef(Args &&... args) {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    };
+
+    template<typename T>
+    using Scope = std::unique_ptr<T>;
+
+    template<typename T, typename... Args>
+    constexpr Scope<T> CreateScope(Args &&... args) {
+        return std::make_unique<T>(std::forward<Args>(args)...);
+    }
 }
