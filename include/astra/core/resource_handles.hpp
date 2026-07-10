8b44602dee0d38d204a427dfd6528fbfb699c6c6
shubham <shubham2010gupta.dev@gmail.com>
2026-07-10 20:14:25 +0530

    feat(core): add basic operators in Handle

--- include/astra/core/resource_handles.hpp
+++ include/astra/core/resource_handles.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 namespace astra::core {
     struct TextureTag {
     };
@@ -14,6 +16,22 @@ namespace astra::core {
     struct Handle {
         uint32_t id = 0;
         uint32_t generation = 0;
+
+        constexpr explicit operator bool() const {
+            return id != 0;
+        }
+
+        constexpr bool operator!() const noexcept {
+            return id == 0;
+        }
+
+        constexpr bool operator==(const Handle &other) const noexcept {
+            return id == other.id && generation == other.generation;
+        }
+
+        constexpr bool operator!=(const Handle &other) const noexcept {
+            return !(*this == other);
+        }
     };
 
     using ShaderHandle = Handle<ShaderTag>;
